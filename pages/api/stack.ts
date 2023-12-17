a4b48ac24c561f894ad2f5c46fd8e99e175f2467
Kayxue <s0990006@gmail.com>
2023-12-17 18:27:40 +0800

    2023/12/17 Change to Nextjs API

--- pages/api/stack.ts
+++ pages/api/stack.ts
@@ -1,11 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import StackExample from "../../public/StackExample.json"
+import Cors from "cors"
 
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await runMiddleware(req, res, cors)
 
   res.status(200).send({...StackExample})
 }
