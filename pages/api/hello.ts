44e1ca5950e7af88837e825af265bc8cc5fecf5b
Kayxue <s0990006@gmail.com>
2023-12-17 18:16:41 +0800

    2023/12/17 Prepareing push to Vercel

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,11 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { promises as fs } from 'fs';
+import StackExample from "../../public/StackExample.json"
 
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const file = await fs.readFile(process.cwd() + '/public/StackExample.json', 'utf8');
-  res.status(200).send(file)
+
+  res.status(200).send({...StackExample})
 }
