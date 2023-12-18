8123e695411d12eea4233552e2b9e06ce5bf0ee0
KayXue <s0990006@gmail.com>
2023-12-18 19:45:19 +0800

    2023/12/18 Remove Vercel speed insight

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { injectSpeedInsights } from "@vercel/speed-insights"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  injectSpeedInsights({})
   await app.listen(3000);
 }
 bootstrap();
