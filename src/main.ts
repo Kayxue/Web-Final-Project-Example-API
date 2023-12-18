ed8a6e7de9370d5324b7ed16b60cabda79e4cd17
KayXue <s0990006@gmail.com>
2023-12-18 19:39:01 +0800

    2023/12/18 Migrate to NestJs, deploy to vercel, and add insight

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { injectSpeedInsights } from "@vercel/speed-insights"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  injectSpeedInsights({})
   await app.listen(3000);
 }
 bootstrap();
